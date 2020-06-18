19e11026f2365a3b39bcc834ac294a29170fd08c
Jiayi Ren <jiayiren22@gmail.com>
2020-06-18 15:59:11 -0700

    complete MVPs

---
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/ShoppingcartApplication.java
@@ -0,0 +1,22 @@
+package com.lambdaschool.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to start the application.
+ */
+
+@SpringBootApplication
+public class ShoppingcartApplication {
+    /**
+     * Main method to start the application.
+     *
+     * @param args Not used in this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingcartApplication.class,
+                args);
+    }
+
+}
